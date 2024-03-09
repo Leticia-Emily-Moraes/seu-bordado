6ba8279fcd0e488c97050118145da1779743ac5e
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2024-03-09 10:32:23 -0300

    feat: added to "apresentacao" section in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/';
+import Apresentacao from './components/apresentacao';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Apresentacao />
     </div>
   );
 }
