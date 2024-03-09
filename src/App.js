fd2a94ec82df950c7e82ec7e08a4962adf8fa268
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2024-03-09 16:50:52 -0300

    feat: Added carousel to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header/';
 import Apresentacao from './components/apresentacao';
+import CarouselDeImgs from './components/Carrousel/';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Apresentacao />
+      <CarouselDeImgs />
     </div>
   );
 }
