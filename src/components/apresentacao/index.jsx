9db8aafe91222eaeba4314678af5bf6077463b7b
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2024-03-09 09:29:00 -0300

    feat: created the structure of the presentation section

---
+++ src/components/apresentacao/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./style.css";
+
+export default function Apresentacao() {
+    return (
+        <section>
+            <aside>
+                <img src="" alt="" />
+            </aside>
+            <aside>
+                <span>
+                    <h2>Pense</h2>
+                    <h2>Crie</h2>
+                    <h2>Decore</h2>
+                </span>
+                <span>
+                    <p>Pense em seu bordado e mande para nos para transformaremos em realidade</p>
+                    <p>Começe seu processo criativo em nossa loja de materiais</p>
+                    <p>Decore com sua obra de arte ou com as obras da nossa galeria </p>
+                </span>
+            </aside>
+        </section>
+    );
+}
