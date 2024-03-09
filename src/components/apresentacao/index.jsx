826b4f4404f583104f888600282966992bb8cb80
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2024-03-09 10:37:20 -0300

    feat: added banner and classes and IDS

--- src/components/apresentacao/index.jsx
+++ src/components/apresentacao/index.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import "./style.css";
+import Banner from "../../assets/img/banners/BannerApresentacao.svg";
 
 export default function Apresentacao() {
     return (
-        <section>
-            <aside>
-                <img src="" alt="" />
+        <section id="apresentacao">
+            <aside className="sectionImg">
+                <img src={Banner} alt="" />
             </aside>
-            <aside>
-                <span>
+            <aside className="sectionTexto">
+                <span className="titulos">
                     <h2>Pense</h2>
                     <h2>Crie</h2>
                     <h2>Decore</h2>
                 </span>
-                <span>
+                <span className="descricoes">
                     <p>Pense em seu bordado e mande para nos para transformaremos em realidade</p>
                     <p>Começe seu processo criativo em nossa loja de materiais</p>
                     <p>Decore com sua obra de arte ou com as obras da nossa galeria </p>
