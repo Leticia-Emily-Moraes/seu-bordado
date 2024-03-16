60f44197529f333c5b80a9690b14c916892a8016
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2024-03-16 16:05:37 -0300

    feat: created mock.js file with Card component information

---
+++ src/components/planCards/mock.js
@@ -0,0 +1,54 @@
+export const CardOne = {
+    positive: [
+        "Tutorias de básico ao Avançado",
+        "Desconto em materiais"
+    ],
+    negative: [
+        "2 Gráficos Personalizados  por mês",
+        "Prioridade no suporte",
+        "Prioridade no personalizado"
+    ]
+}
+export const CardTwo = {
+    positive: [
+        "Tutorias de básico",
+        "5% Desconto em quadros",
+        "2 Gráficos Personalizados por mês"
+    ],
+    negative: [
+        "Prioridade no suporte",
+        "Prioridade no personalizado"
+    ]
+}
+export const CardThree = {
+    positive: [
+        "Tutorias de básico ao Intermediário",
+        "10% Desconto em quadros",
+        "5 Gráficos Personalizados por mês",
+        "Prioridade no suporte"
+    ],
+    negative: [
+        "Prioridade no personalizado"
+    ]
+}
+
+export const CardFour = {
+    positive: [
+        "Tutorias de básico ao Avançado",
+        "15% Desconto em quadros",
+        "10 Gráficos Personalizados por mês",
+        "Prioridade no suporte",
+        "Prioridade no personalizado"
+    ],
+    negative: [
+    ]
+}
+
+
+// export const messageCard = [
+//     CardOne,
+//     CardTwo,
+//     CardThree,
+//     CardFour
+// ]
+
